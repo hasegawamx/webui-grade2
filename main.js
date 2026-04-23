3536dbac695d46265d384f7f84cc7a282b9787f3
*0O0cafAJ6q <hasegawa@rezavex.com>
2026-04-23 16:01:18 +0700

    Add counter functions

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-// const name = prompt("名前は？");
-// alert("こんにちは " + name + " さん！");
-
-// const target = "<h1 id="target">Hello world</h1>";
-const target = document.getElementById("target");
-target.textContent = "こんにちは";
-target.style.color = "red";
-target.style.fontSize = "10px";
-target.style.backgroundColor = "yellow";
+function sayHello() {
+    alert("Hello");
+}
